6c0cd547930b9abb7f1720412297673ec6a5005b
john connolly <jtconnolly82@gmail.com>
2020-02-23 21:51:35 -0700

    mutation from frontend

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,9 +1,102 @@
 import React from "react";
 
 class Auth extends React.Component {
+
+  state = {
+    isLoggedIn: true,
+  }
+
+
+  constructor(props) {
+    super(props);
+    this.emailElement = React.createRef();
+    this.passwordElement = React.createRef();
+  }
+  
+  
+  handleButtonSwitch = () => {
+    this.setState(prev => {
+      return {
+        isLoggedIn: !prev.isLoggedIn
+      }
+    })
+  }
+  
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const email = this.emailElement.current.value;
+    const password = this.passwordElement.current.value;
+
+    if (email.trim().length === 0 || password.trim().length === 0) {
+      return;
+    }
+
+    let requestBody = {
+      query: `
+        query {
+          login(email: "${email}", password: "${password}") {
+            userId
+            token
+            tokenExpiration
+          }
+        }
+      `
+    };
+
+    if (!this.state.isLoggedIn) {
+      requestBody = {
+        "query": `
+          mutation {
+            createUser(userInput: {email: "${email}", password: "${password}"}) {
+              _id
+              email
+            }
+          }
+        `
+      };
+    }
+
+    fetch('http://localhost:3030/graphql', {
+      method: 'POST',
+      body: JSON.stringify(requestBody),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(res => {
+        if (res.status !== 200 && res.status !== 201) {
+          throw new Error('Failed!');
+        }
+        return res.json();
+      })
+      .then(resData => {
+        console.log(resData);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
   render() {
     return (
-    <h1>Auth</h1>
+    <>
+      <h1>Auth</h1>
+      <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor="email">email</label>
+            <input type='email' id='email' ref={this.emailElement} />
+          </div>
+          <div>
+            <label htmlFor="password">password</label>
+            <input name='password' id='password' ref={this.passwordElement} />
+          </div>
+          <div>
+          <button type='button' onClick={this.handleButtonSwitch} >Switch to {this.state.isLoggedIn ? 'signup' : 'login'}</button>
+          <button type='submit'>submit</button>
+          </div>
+      </form>
+    </>
     )
   }
 }
--- server.js
+++ server.js
@@ -1,38 +1,49 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
 const app = express();
-const mongoose = require('mongoose');
-require('dotenv').config();
+const mongoose = require("mongoose");
+require("dotenv").config();
 
-const gqlResolvers = require('./graphql/resolvers/index');
-const gqlSchema = require('./graphql/schema/index');
-const authMiddle = require('./middleware/authMiddle');
+const gqlResolvers = require("./graphql/resolvers/index");
+const gqlSchema = require("./graphql/schema/index");
+const authMiddle = require("./middleware/authMiddle");
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS");
+  res.setHeader("Access-Control-ALlow-Headers", "Content-Type, Authorization");
+  if ((req.method === "OPTIONS")) {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 app.use(authMiddle);
 
 
-app.use('/graphql', graphqlHTTP({
-  schema: gqlSchema,
-  rootValue: gqlResolvers,
-   graphiql: true,
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: gqlSchema,
+    rootValue: gqlResolvers,
+    graphiql: true
   })
 );
 
+app.get("/", (req, res, next) => {
+  res.send("hi ✋");
+});
 
-
-app.get('/', (req, res, next) => {
-  res.send('hi ✋');
-})
-
-mongoose.connect(`mongodb+srv://john:${process.env.MONGO_PW}@cluster0-6g6x4.mongodb.net/event-gql?retryWrites=true&w=majority`,
-{ useNewUrlParser: true, useUnifiedTopology: true })
-  .then(()=> {
-    console.log('db connected on mongodb atlas')
+mongoose
+  .connect(
+    `mongodb+srv://john:${process.env.MONGO_PW}@cluster0-6g6x4.mongodb.net/event-gql?retryWrites=true&w=majority`,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log("db connected on mongodb atlas");
   })
   .catch(err => {
     console.dir(err);
-  })
+  });
 
-
-  app.listen(3030);
+app.listen(3030);
